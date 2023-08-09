b512082e40df5d0464d713c435b629e8bbaebab1
Monika Janas <monika.janas@microsoft.com>
2023-08-09 12:03:22 -0700

    Push changes for simple pf segments api planner

--- dotnet/samples/KernelSyntaxExamples/Example00_03_OpenApiSkill_PlayFab.cs
+++ dotnet/samples/KernelSyntaxExamples/Example00_03_OpenApiSkill_PlayFab.cs
@@ -2,10 +2,15 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.AI.TextCompletion;
 using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel.Planning.Stepwise;
 using Microsoft.SemanticKernel.SkillDefinition;
 using Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
 using Microsoft.SemanticKernel.Skills.OpenAPI.Extensions;
@@ -20,7 +25,26 @@ public static class Example_00_03_OpenApiSkill_PlayFab
 {
     public static async Task RunAsync()
     {
-        await SkillImportExample();
+        // await SkillImportExample();
+
+        string[] questions = new string[]
+        {
+            // "Get the details for PlayFab segment 968016902A4DC242",
+            "Get all my PlayFab segments",
+            // "Create a segment named 'My Favorite Segment' for players who last logged in over 1 day ago",
+        };
+
+        foreach (string q in questions)
+        {
+            try
+            {
+                await PlannerExample(q);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
     }
 
     private static async Task SkillImportExample()
@@ -49,6 +73,48 @@ public static class Example_00_03_OpenApiSkill_PlayFab
         }
     }
 
+    private static async Task PlannerExample(string question)
+    {
+        var kernel = new KernelBuilder()
+            .WithLogger(ConsoleLogger.Logger)
+            .WithAzureChatCompletionService(TestConfiguration.AzureOpenAI.ChatDeploymentName, TestConfiguration.AzureOpenAI.Endpoint, TestConfiguration.AzureOpenAI.ApiKey, alsoAsTextCompletion: true, setAsDefault: true)
+            .Build();
+
+        SKContext context = kernel.CreateNewContext();
+
+        context.Variables.Set("content_type", "application/json");
+
+        using HttpClient httpClient = new();
+
+        var playfabApiSkills = await GetPlayFabSkill(kernel, httpClient);
+        //kernel.ImportSkill(new ExtractHexIdSkill(), "ExtractHexadecimalId");
+
+        var plannerConfig = new StepwisePlannerConfig();
+        plannerConfig.ExcludedFunctions.Add("TranslateMathProblem");
+        plannerConfig.MinIterationTimeMs = 1500;
+        plannerConfig.MaxTokens = 1000;
+        plannerConfig.MaxIterations = 5;
+
+        StepwisePlanner planner = new(kernel, plannerConfig);
+        Plan plan = planner.CreatePlan(question);
+
+        var settings = new CompleteRequestSettings { Temperature = 0.1, MaxTokens = 250 };
+        var result = await plan.InvokeAsync(context, settings);
+
+        Console.WriteLine("Question: " + question);
+
+        Console.WriteLine("Result: " + result);
+        if (result.Variables.TryGetValue("stepCount", out string? stepCount))
+        {
+            Console.WriteLine("Steps Taken: " + stepCount);
+        }
+
+        if (result.Variables.TryGetValue("skillCount", out string? skillCount))
+        {
+            Console.WriteLine("Skills Used: " + skillCount);
+        }
+    }
+
     private static async Task<IDictionary<string, ISKFunction>> GetPlayFabSkill(IKernel kernel, HttpClient httpClient)
     {
         IDictionary<string, ISKFunction> playfabApiSkills;
@@ -63,7 +129,9 @@ public static class Example_00_03_OpenApiSkill_PlayFab
         if (useLocalFile)
         {
             var playfabApiFile = "../../../Skills/PlayFabApiSkill/openapi.json";
-            playfabApiSkills = await kernel.ImportOpenApiSkillFromFileAsync("PlayFabApiSkill", playfabApiFile, new OpenApiSkillExecutionParameters(httpClient, authCallback: titleSecretKeyProvider.AuthenticateRequestAsync)); ;
+            var parameters = new OpenApiSkillExecutionParameters(httpClient, authCallback: titleSecretKeyProvider.AuthenticateRequestAsync, serverUrlOverride: new Uri(TestConfiguration.PlayFab.Endpoint));
+
+            playfabApiSkills = await kernel.ImportOpenApiSkillFromFileAsync("PlayFabApiSkill", playfabApiFile, parameters); ;
         }
         else
         {
