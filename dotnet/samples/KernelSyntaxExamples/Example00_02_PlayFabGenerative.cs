042b27852ba7a3a287abd69a94d60b6661e73edc
Rosi Reddy Rangareddy <rosir@microsoft.com>
2023-08-09 20:03:45 -0500

    Making restructing changes

--- dotnet/samples/KernelSyntaxExamples/Example00_02_PlayFabGenerative.cs
+++ dotnet/samples/KernelSyntaxExamples/Example00_02_PlayFabGenerative.cs
@@ -1,12 +1,93 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.SkillDefinition;
+using Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
+using Microsoft.SemanticKernel.Skills.OpenAPI.Extensions;
+using RepoUtils;
+using Microsoft.SemanticKernel.Planning;
+using Newtonsoft.Json;
+using Microsoft.SemanticKernel.Skills.Core;
 
 public static class Example00_02_PlayFabGenerative
 {
     public static async Task RunAsync()
     {
-        throw new NotImplementedException();
+        var goals = new string[]
+            {
+                "Create a segment with name NewPlayersSegment for the players first logged in date greater than 2023-08-01?",
+                "Create a segment with name LegacyPlayersSegment for the players last logged in date less than 2023-05-01?",
+                "Create a segment with name EgyptNewPlayers for the players located in the Egypt?",
+                "Create a segment with name WelcomeEgyptNewPlayers for the players located in the Egypt with entered segment action of email notification?", // With entered segment action
+                "Create a segment with name EgyptNewPlayers for the players located in the Egypt?" // If the segment already exist, create a segment with name appended with guid
+            };
+        await CreateSegmentExample(goals[0]);
+        //await GetSegmentsExample();
+    }
+
+    private static async Task CreateSegmentExample(string goal)
+    {
+        using HttpClient httpClient = new();
+
+        // Create a segment skill
+        {
+            Console.WriteLine("======== Action Planner ========");
+            var kernel2 = new KernelBuilder()
+                .WithLogger(ConsoleLogger.Logger)
+                .WithAzureTextCompletionService("text-davinci-003", TestConfiguration.AzureOpenAI.Endpoint, TestConfiguration.AzureOpenAI.ApiKey) // Note: Action Planner works with old models like text-davinci-002
+                .Build();
+
+            string folder = RepoFiles.SampleSkillsPath();
+            kernel2.ImportSkill(new SegmentSkill(), "SegmentSkill");
+
+            // Create an instance of ActionPlanner.
+            // The ActionPlanner takes one goal and returns a single function to execute.
+            var planner = new ActionPlanner(kernel2);
+
+            // We're going to ask the planner to find a function to achieve this goal.
+            //var goal = "Create a segment with name NewPlayersSegment for the players first logged in date greater than 2023-08-01?";
+            Console.WriteLine("Goal: " + goal);
+
+            // The planner returns a plan, consisting of a single function
+            // to execute and achieve the goal requested.
+            var plan = await planner.CreatePlanAsync(goal);
+            plan.Steps[0].Parameters = plan.Parameters;
+
+            // Execute the full plan (which is a single function)
+            SKContext result = await plan.InvokeAsync(kernel2.CreateNewContext());
+
+            // Show the result, which should match the given goal
+            Console.WriteLine(result);
+        }
+    }
+
+    private static async Task<IDictionary<string, ISKFunction>> GetPlayFabSkill(IKernel kernel, HttpClient httpClient)
+    {
+        IDictionary<string, ISKFunction> playfabApiSkills;
+
+        var titleSecretKeyProvider = new PlayFabAuthenticationProvider(() =>
+        {
+            string s = TestConfiguration.PlayFab.TitleSecretKey;
+            return Task.FromResult(s);
+        });
+
+        bool useLocalFile = true;
+        if (useLocalFile)
+        {
+            var playfabApiFile = "../../../Skills/PlayFabApiSkill/openapi.json";
+            playfabApiSkills = await kernel.ImportOpenApiSkillFromFileAsync("PlayFabApiSkill", playfabApiFile, new OpenApiSkillExecutionParameters(httpClient, authCallback: titleSecretKeyProvider.AuthenticateRequestAsync));
+        }
+        else
+        {
+            var playfabApiRawFileUrl = new Uri(TestConfiguration.PlayFab.SwaggerEndpoint);
+            playfabApiSkills = await kernel.ImportOpenApiSkillFromUrlAsync("PlayFabApiSkill", playfabApiRawFileUrl, new OpenApiSkillExecutionParameters(httpClient, authCallback: titleSecretKeyProvider.AuthenticateRequestAsync));
+        }
+
+        return playfabApiSkills;
     }
 }
